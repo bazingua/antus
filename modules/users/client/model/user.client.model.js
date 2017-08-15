512f5cb2cab658674dae7aa79d0e142a3f54523c
Khadime Wade <khadimewade27@gmail.com>
2017-08-15 21:33:46 +0000

    Khadime model user

--- modules/users/client/model/user.client.model.js
+++ modules/users/client/model/user.client.model.js
@@ -13,6 +13,16 @@ angular.module('demandes.model').factory('ProjetModel', [
     function ProjetModel(data) {
       data = data || {};
       this.id= data.id || 0;
+      this.prenom = data.prenom;
+      this.nom = data.nom;
+      this.civilite = data.civilite;
+      this.dateNassance=data.dateNassance;
+      this.nationalite=data.nationalite;
+      this.adresse=data.adresse;
+      this.age=data.age;
+      this.sexe=data.sexe;
+      this.telephone=data.telephone;
+      this.active=data.active;
     }
 
     return ProjetModel;
