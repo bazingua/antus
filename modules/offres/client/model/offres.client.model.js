be7b6da8368c6888d34ae08890b6f74fcdc386b8
Khadime Wade <khadimewade27@gmail.com>
2017-09-27 00:29:02 +0000

    commit for offre module

--- modules/offres/client/model/offres.client.model.js
+++ modules/offres/client/model/offres.client.model.js
@@ -1,9 +1,9 @@
-angular.module('offres.model').factory('OffresModel', [
+angular.module('offres.model').factory('OffresModel', ['BanqueModel',
   /**
    * @description offres model
    * @returns {OffresModel}
    */
-  function () {
+  function (BanqueModel) {
     'use strict';
     /**
      *Descript: model's contructor
@@ -13,6 +13,15 @@ angular.module('offres.model').factory('OffresModel', [
     function OffresModel(data) {
       data = data || {};
       this.id = data.id || 0;
+      this.banque = new BanqueModel(data.banque);
+      this.montantPret = data.montantPret || 0;
+      this.dureePret = data.dureePret || 0;
+      this.tauxInteret = data.tauxInteret || 0;
+      this.echeance = data.echeance || 0;
+      this.fraisDossier = data.fraisDossier || 0;
+      this.assuranceOffre = data.assuranceOffre || 0;
+      this.fraisNotaire = data.fraisNotaire || 0;
+      this.dateCreation = data.dateCreation || "";
     }
 
     return OffresModel;
