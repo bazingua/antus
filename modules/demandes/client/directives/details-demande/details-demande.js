b7b7c774e0bcd21d25cf2e0f7766f2c72499c05a
Hakim SOCK <serigne.sock@neticoa.fr>
2017-10-24 01:28:55 +0000

    feat(demande): directive list and details demande

---
+++ modules/demandes/client/directives/details-demande/details-demande.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular.module('demandes.directives')
+        .directive('detailsDemande', detailsDemande);
+
+    detailsDemande.$inject = [];
+
+    function detailsDemande() {
+        var directive = {
+            restrict: 'EA',
+            transclude: false,
+            templateUrl: 'modules/demandes/client/directives/details-demande/details-demande.html',
+            scope: {
+              demande: '='
+            },
+            link: link
+        };
+
+        return directive;
+        /**
+         * link function
+         * @param  {Object} scope
+         * @return {Object} scope
+         */
+        function link(scope) {
+            console.log(scope.demande);
+        }
+    }
+}());
