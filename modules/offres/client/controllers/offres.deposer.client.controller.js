e29acdc7ea0deedf61ba2e9185de1f265e1bca41
Hakim SOCK <serigne.sock@neticoa.fr>
2017-11-01 02:11:27 +0000

    feat(demande): deposer une demande ans redirect on HomeClient

---
+++ modules/offres/client/controllers/offres.deposer.client.controller.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  // Offres controller
+  angular
+    .module('offres')
+    .controller('OffresDeposerController', OffresDeposerController);
+
+  OffresDeposerController.$inject = ['$scope', '$state', '$stateParams', '$window', 'Authentication', 'OffresModel', 'DemandesService'];
+
+  function OffresDeposerController ($scope, $state, $stateParams, $window, Authentication, OffresModel, DemandesService) {
+    var vm = this;
+    vm.authentication = Authentication;
+    vm.offre = new OffresModel();
+    vm.offre.demandeId = $stateParams.demandeId;
+    vm.error = null;
+
+    // Save Offre
+    vm.deposerOffre = function (isValid) {
+      isValid = true;
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.form.offreForm');
+        return false;
+      }
+
+      DemandesService.deposerOffre(vm.offre.demandeId, vm.offre)
+      .then(successCallback)
+      .catch(errorCallback);
+
+      function successCallback(res) {
+        console.log('res', res);
+        $state.go('demandes.view', {
+          demandeId: vm.offre.demandeId
+        });
+      }
+
+      function errorCallback(error) {
+        console.log('error', error);
+        vm.error = error.data.message;
+      }
+    }
+  }
+}());
