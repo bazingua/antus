7be1e8aec8451016b3e06a264d248f88b0224a8c
Hakim SOCK <hsock@localhost.localdomain>
2018-08-18 22:14:54 +0000

    feat(liste demande): ajout du filtre

--- modules/demandes/client/directives/list-demandes/list-demandes.js
+++ modules/demandes/client/directives/list-demandes/list-demandes.js
@@ -24,6 +24,7 @@
      */
     function link(scope) {
       console.log('++++', scope.demandes);
+      scope.isAdmin = Utils.isAdmin();
       // model filtre demande
       scope.filtre = {
         demande: {
